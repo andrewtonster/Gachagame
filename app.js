99cea27f72849a638d7e52f4cd79ae50f37ed095
andrewtonster <andrewtonster@gmail.com>
2022-12-15 18:43:56 -0800

    added function to prit all usersand username checking

--- app.js
+++ app.js
@@ -2,10 +2,22 @@ const express = require("express");
 const app = express();
 const ejs = require("ejs");
 const router = require("./routes/route");
+const mongoose = require("mongoose");
 
 //STATIC FILES
 // this is middleware
 // this will serve the static files in the public class
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect("mongodb://localhost/gacha")
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
 app.use(express.static("public"));
 app.use("/css", express.static(__dirname + "public/css"));
 app.use("/img", express.static(__dirname + "public/img"));
--- routes/route.js
+++ routes/route.js
@@ -1,18 +1,40 @@
 const bodyParser = require("body-parser");
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 const router = express.Router();
+const User = require("../models/users");
 
 router.get("/", (req, res) => {
   res.render("main");
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const { username } = req.body;
   const { password } = req.body;
 
-  console.log(username);
-  console.log(password);
+  if (checkLogin(username, password) === true) {
+    console.log("User already in use");
+  } else {
+    const user = await User.create({
+      username: username,
+      password: password,
+    });
+  }
 });
+
+const checkLogin = (user, pass) => {
+  if (User.find({ username: user })) {
+    return true;
+  }
+};
+
+const printAll = () => {
+  User.find().then((users) => {
+    console.log(users);
+  });
+};
+
+printAll();
 
 // exports the routers
 module.exports = router;
